4ef3e13c91136220a4b132e9d80ae9e340f012f4
harika517 <hharika38@gmail.com>
2020-12-01 09:35:11 -0800

    mongoDB connection setup

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db')
 const app = express();
+
+//connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'))
 const PORT = process.env.PORT || 3001;
