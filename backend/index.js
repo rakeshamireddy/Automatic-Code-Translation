27bddace11a1eed0fba0628e784f1c03c330bfe2
harika517 <hharika38@gmail.com>
2020-12-01 09:24:58 -0800

    Node Express server setup

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'))
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
