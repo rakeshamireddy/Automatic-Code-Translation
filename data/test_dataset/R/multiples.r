b5db4a8818324a95f35c320f4574cdc1cf217bbc
Nihanjali Mallavarapu <55064219+nihanjali@users.noreply.github.com>
2021-03-01 14:34:24 -0800

    Multiples of entered number

---
+++ data/test_dataset/R/multiples.r
@@ -0,0 +1,7 @@
+# R Program to find the multiplicationtable (from 1 to 10)
+# take input from the user
+num = 12
+# use for loop to iterate 10 times
+for(i in 1:10) {
+print(paste(num,'x', i, '=', num*i))
+}
