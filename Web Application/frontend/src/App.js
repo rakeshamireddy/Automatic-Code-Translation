6083a1285974ae971df083de97073c0305ea42e6
Harika <hharika38@gmail.com>
2021-04-02 22:19:26 -0700

    initial components

--- Web Application/frontend/src/App.js
+++ Web Application/frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, {Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 
 const App = () =>
     <Fragment>
-      <h1> App </h1>
-      
+      <Navbar/>
+      <Landing/>
     </Fragment>
 
 export default App;
