e6b55480c1edf0f78daf99b826814514717c97b2
Harika <hharika38@gmail.com>
2021-04-21 15:08:34 -0700

    Load User and set Auth Token

--- Web Application/frontend/src/App.js
+++ Web Application/frontend/src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
-import React, {Fragment} from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React, {Fragment, useEffect} from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+
 //Redux
 import {Provider} from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () =>(
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+const App = () =>{ 
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  
+  return (
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -27,7 +39,7 @@ const App = () =>(
     </Router>
 
   </Provider>
-)
+)}
 
     
 
--- Web Application/frontend/src/actions/auth.js
+++ Web Application/frontend/src/actions/auth.js
@@ -1,7 +1,28 @@
 import axios from 'axios'
+import setAuthToken from '../utils/setAuthToken'
 import {setAlert} from './alert'
 import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR} from './types'
 
+//Load User 
+export const loadUser = () => async dispatch => {
+  if(localStorage.token){
+    setAuthToken(localStorage.token);
+  }
+
+  try{
+    const res = await axios.get('/api/auth');
+    dispatch ({
+      type: USER_LOADED,
+      payload: res.data
+    })
+
+  }catch(err){
+    dispatch ({
+      type: AUTH_ERROR
+    })
+
+  }
+}
 //Register User
 export const register = ({name, email, password}) => async dispatch =>{
   const config = {
