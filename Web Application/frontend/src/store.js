e0a5c7c762c226d087a637f663eb47178ee387f6
Harika <hharika38@gmail.com>
2021-04-20 18:06:38 -0700

    connected redux store to react components

--- Web Application/frontend/src/store.js
+++ Web Application/frontend/src/store.js
@@ -1,4 +1,4 @@
-import {creatStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
@@ -6,6 +6,6 @@ import rootReducer from './reducers';
 const initialState = {};
 
 const middleware =[thunk];
-const store = creatStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
