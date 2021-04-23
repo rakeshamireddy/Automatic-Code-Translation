a0112729b5497854350ec1fe93db5558107cf4d4
Harika <hharika38@gmail.com>
2021-04-22 20:55:07 -0700

    Initial Translation form

--- Web Application/frontend/src/components/dashboard/Dashboard.js
+++ Web Application/frontend/src/components/dashboard/Dashboard.js
@@ -11,10 +11,38 @@ const Dashboard = ({auth: {user, loading}}) => {
     <div className = "container">
       <div className = "container_2columns">
         <div className = "column1">
-            <p>File upload</p>
+            <p className="large"><i className = "far fa-file-alt"></i></p>
+            <p>Upload Your Source Code</p>
+            <br/>
+            <br/>
+            <form>
+              <div className = "file-field input-field">
+                <div className = "btn #64b5f6 blue darken-1">
+                  {/* <span> Upload program file</span>  */}
+                  <input type = "file"></input>
+                </div>
+              </div>
+              <br/>
+              <button type = "submit" className = "btn btn-dark"> Upload file</button>
+            </form>
         </div>
         <div className = "column2">
-          <p> File convert</p>
+          <p> Choose your target language for conversion </p>
+          <br/>
+          <br/>
+          <form>
+            <select clasName="language-dropdown" id="#" name="#">
+              <option value="" selected disabled hidden>Choose language</option>
+              <option value="Python">Python</option>
+              <option value="Java">Java</option>
+              <option value="C++">C++</option>
+              <option value="R">R</option>
+            </select>
+            <br/>
+            <br/>
+            <button type = "submit" className = "btn btn-dark"> Translate</button>
+          </form>
+
         </div>
       </div>
     </div>
