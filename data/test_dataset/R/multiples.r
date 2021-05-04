0400d6c03806c60fcf2e8b145d6aed8e67ef02ba
Nihanjali Mallavarapu <55064219+nihanjali@users.noreply.github.com>
2021-05-03 22:14:41 -0700

    Update multiples.r

--- data/test_dataset/R/multiples.r
+++ data/test_dataset/R/multiples.r
@@ -1,7 +1,5 @@
-# R Program to find the multiplicationtable (from 1 to 10)
-# take input from the user
-num = 12
-# use for loop to iterate 10 times
+multiples <- function() {
+
 for(i in 1:10) {
-print(paste(num,'x', i, '=', num*i))
+return(num*i)
 }
