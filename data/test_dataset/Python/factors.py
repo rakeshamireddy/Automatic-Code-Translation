996bd9eb4e448e3a3fb13b5e92ce97097bd7f593
Nihanjali Mallavarapu <55064219+nihanjali@users.noreply.github.com>
2021-05-03 21:48:55 -0700

    Update factors.py

--- data/test_dataset/Python/factors.py
+++ data/test_dataset/Python/factors.py
@@ -1,12 +1,5 @@
-# Python Program to find the factors of a number
-
-# This function computes the factor of the argument passed
 def print_factors(x):
-   print("The factors of",x,"are:")
+   print('The factors of',x,'are:')
    for i in range(1, x + 1):
        if x % i == 0:
            print(i)
-
-num = 320
-
-print_factors(num)
