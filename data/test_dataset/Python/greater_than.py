9cbac351ff55f70f984407609e78cccb55e2daa8
Gayathri Ganesh <47293060+gayathrig269@users.noreply.github.com>
2021-05-03 23:00:29 -0700

    Add files via upload

---
+++ data/test_dataset/Python/greater_than.py
@@ -0,0 +1,4 @@
+def greater_than():
+  a = 10
+  b = 2
+  return True if a > b else False
---
+++ data/test_dataset/Python/log_operations.py
@@ -0,0 +1,4 @@
+def log_operations():
+  x = [True,False,0,6]
+  y = [False,True,False,True]
+  return(x or y)
