e10089811aa45a9e1e9dec2bb5223eafee01a507
Nihanjali Mallavarapu <55064219+nihanjali@users.noreply.github.com>
2021-05-03 22:00:19 -0700

    Update triangle.py

--- data/test_dataset/Python/triangle.py
+++ data/test_dataset/Python/triangle.py
@@ -1,12 +1,8 @@
-# Python Program to find the area of triangle
-
 a = 5
 b = 6
 c = 7
 
-# calculate the semi-perimeter
 s = (a + b + c) / 2
 
-# calculate the area
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
 print('The area of the triangle is %0.2f' %area)
