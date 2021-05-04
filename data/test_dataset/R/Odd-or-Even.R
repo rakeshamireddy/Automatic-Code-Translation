95084347a41de944eb8ca449dfcfeb1ac621c858
Nihanjali Mallavarapu <55064219+nihanjali@users.noreply.github.com>
2021-05-03 22:03:59 -0700

    Update Odd-or-Even.R

--- data/test_dataset/R/Odd-or-Even.R
+++ data/test_dataset/R/Odd-or-Even.R
@@ -1,13 +1,8 @@
-# Program to check if the input number is odd or even.
-# A number is even if division by 2 give a remainder of 0.
-# If remainder is 1, it is odd.
 odd_or_even <- function(n){
 num = n
 if((num %% 2) == 0) {
-  print(paste(num,"is Even"))
+  print('even')
 } else {
-  print(paste(num,"is Odd"))
+  print('is Odd')
 }
 }
-
-odd_or_even(4)
